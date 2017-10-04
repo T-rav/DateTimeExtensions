516ee69629d66d82a9573cde0755e3ff091c69ca
T-rav <travis.frisinger@chillisoft.co.za>
2017-10-04 10:20:26 +0200

    Added projects

---
+++ TddBuddy.DateTime.Extensions/TddBuddy.DateTime.Extensions.Tests/DateTimeExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace TddBuddy.DateTime.Extensions.Tests
+{
+    [TestFixture]
+    public class DateTimeExtensionsTests
+    {
+        [Test]
+        public void ConvertTo24HourFormatWithSeconds_WhenDateTimePmValue_ShouldReturnStringWithSeconds()
+        {
+            //---------------Arrange-------------------
+            var dateTime = new System.DateTime(2017,2,1,22,00,01);
+            //---------------Act----------------------
+            var result = dateTime.ConvertTo24HourFormatWithSeconds();
+            //---------------Assert-----------------------
+            Assert.AreEqual("2017-02-01 22:00:01",result);
+        }
+
+        [Test]
+        public void ConvertTo24HourFormatWithSeconds_WhenDateTimeAmValue_ShouldReturnStringWithSeconds()
+        {
+            //---------------Arrange-------------------
+            var dateTime = new System.DateTime(2017, 2, 1, 10, 05, 09);
+            //---------------Act----------------------
+            var result = dateTime.ConvertTo24HourFormatWithSeconds();
+            //---------------Assert-----------------------
+            Assert.AreEqual("2017-02-01 10:05:09", result);
+        }
+    }
+}
---
+++ TddBuddy.DateTime.Extensions/TddBuddy.DateTime.Extensions/DateTimeExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace TddBuddy.DateTime.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ConvertTo24HourFormatWithSeconds(this System.DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+}
